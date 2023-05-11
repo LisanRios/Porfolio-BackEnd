e7213ccc178cfcf31a984da4c6ac6e3c48b82710
Lisandro Rios <rios.lisandro369@gmail,com>
2023-05-11 18:43:30 -0300

    oranizacion update 2

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM amazoncorretto:17
+
+MAINTAINER LisanRios
+
+COPY target/portfolio-0.0.1-SNAPSHOT.jar app.jar
+
+ENTRYPOINT ["java","-jar","/app.jar"]
---
+++ src/main/java/com/LisandroRios/porfolioweb/PorfoliowebApplication.java
@@ -0,0 +1,13 @@
+package com.LisandroRios.porfolioweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PorfoliowebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PorfoliowebApplication.class, args);
+	}
+
+}
